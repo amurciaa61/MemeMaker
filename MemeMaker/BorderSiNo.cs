f8b54957c18f7f36a8705de97d21ff046787da47
Antonio Murcia <amurciaa61@gmail.com>
2020-11-14 00:43:27 +0100

    Creo la clase BorderSiNo, que implementa la interfaz IValueConverter, y la instancio para modificar el BorderThickness

---
+++ MemeMaker/BorderSiNo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class BorderSiNo : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+                return "2";
+            else
+                return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
